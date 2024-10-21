f27ec201e76a49ad13a3cc965c00581074a031d9
Antonio Mendonca <mendonca.livio@gmail.com>
2024-10-21 16:07:14 -0300

    feat: add `.env` and queries for breastcancer

--- examples/breastcancer/main.sql
+++ examples/breastcancer/main.sql
@@ -1,15 +1,30 @@
 -- Get the container up and running before try to run this file
 
 /*
-This file has an example of a possible query with the similarity search
+This file has examples of a possible queries with the similarity search
 */
 
+-- remeber to truncate tables CENTROIDS and CENT_ASSIGN to recalculate clusters
+TRUNCATE centroids, cent_assign;
 
+-- two dimension
 SELECT 
-	spatial_sim(hospital, 3.5) AS space,
+	spatial_sim(hospital, 3.5) AS spatial,
 	metric_sim(id, radius1, texture1, perimeter1, 3) AS metric,
 	count(id)
 FROM breastcancer 
 GROUP BY
-	CUBE(space, metric)
-ORDER BY space, metric ASC;
+	CUBE(spatial, metric)
+ORDER BY spatial, metric ASC;
+
+
+-- three dimension
+SELECT 
+	spatial_sim(hospital, 5) AS spatial,
+	extract(month from time_stamp) as temporal,
+	metric_sim(id, radius1, texture1, perimeter1, 5) AS metric,
+	count(id)
+FROM breastcancer 
+GROUP BY
+	CUBE(spatial, temporal, metric)
+ORDER BY spatial, temporal, metric ASC;
